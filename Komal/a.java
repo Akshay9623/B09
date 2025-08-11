80e0035fc04358d391b86edf26f4105cfe757fd7
Komalggulhane <komalgulhane007@gmail.com>
2025-08-11 13:46:16 +0530

    Commit by Komal

--- Komal/a.java
+++ Komal/a.java
@@ -1,1 +1,13 @@
-sdaiogjefdufjdbdkvjds gieriioewjfioower;jtioq2jtieqrhgiowejhfjadjgfjids
+public class A {
+
+	public static void main(String args[]) {
+		System.out.println("Hello");
+	}
+
+}public class A {
+
+	public static void main(String args[]) {
+		System.out.println("Hello");
+	}
+
+}
